e64673db5e3a28cb1b9e981cebf1441b699c5ce0
Bertan Atalay <bertan@2creative.net>
2016-06-20 13:21:09 +0100

    solas-5223: updating the answer test class name to fix the test

--- test/Answer.test.js
+++ test/Answer.test.js
@@ -5,10 +5,10 @@ import Answer from '../src/Answer'
 
 test('does not have the CSS class `active` by default', t => {
   const w = shallow(<Answer active={false}>An answer</Answer>)
-  t.not(w.find('button').hasClass('active'))
+  t.not(w.find('button').hasClass('rq-Answer-button--active'))
 })
 
 test('the `active` prop sets the CSS class `active`', t => {
   const w = shallow(<Answer active={true}>An answer</Answer>)
-  t.true(w.find('button').hasClass('active'))
+  t.true(w.find('button').hasClass('rq-Answer-button--active'))
 })

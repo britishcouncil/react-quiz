54e5ad30dfec5e34976275ca5de915a4355400a1
Bertan Atalay <bertan@2creative.net>
2016-06-21 14:41:23 +0100

    solas-5224: updating the test for finish button

--- test/Quiz.test.js
+++ test/Quiz.test.js
@@ -126,7 +126,7 @@ test('Quiz button container class name', t => {
   t.true(w.find('.rq-Quiz-buttonContainer').hasClass('some-class-name'))
 })
 
-test.only('Quiz finish button, calls the onFinished function with the answers', t => {
+test('Quiz finish button, calls the onFinished function with the answers', t => {
   const w = shallow(
     <Quiz {...defaultTestProps}
           title="The quiz title"

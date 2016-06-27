ef59e5b3608fd6bc26a5b9ba4a26e81ed6d3d5a4
Robin Edman <robin.edman@gmail.com>
2016-06-27 13:33:18 +0100

    Ability to dynamically pass rq-Answer-button--active className in

--- src/Answer.js
+++ src/Answer.js
@@ -5,7 +5,9 @@ const Answer = ({ active, onClick, children, customClassNames }) => (
   <li className={classNames('rq-Answer', customClassNames['rq-Answer'])}>
     <button onClick={onClick}
             className={classNames('rq-Answer-button', customClassNames['rq-Answer-button'],
-            {'rq-Answer-button--active': active})}>{children}</button>
+              { 'rq-Answer-button--active': active,
+                [customClassNames['rq-Answer-button--active']]: active
+              })}>{children}</button>
   </li>
 )
 

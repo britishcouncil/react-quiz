9e7b478b3031c5302e8c52445b31856d18560bf1
Robin Edman <robin.edman@gmail.com>
2016-06-27 12:54:42 +0100

    Tidy up QuizContainer by passing all props through to Quiz and omitting propTypes
    propTypes are still checked by downstream components, which is a better behaviour.

--- src/QuizContainer.js
+++ src/QuizContainer.js
@@ -27,39 +27,12 @@ class QuizContainer extends React.Component {
   })
 
   render () {
-    return <Quiz title={this.props.title}
-                 questions={this.props.questions}
+    return <Quiz {...this.props}
                  currentQuestionIndex={this.state.currentQuestionIndex}
                  answers={this.state.answers}
                  onAnswer={this.onAnswer}
-                 onNext={this.onNext}
-                 onFinished={this.props.onFinished}
-                 progressTextTemplate={this.props.progressTextTemplate}
-                 progressBarClassName={this.props.progressBarClassName}
-                 progressBarInnerClassName={this.props.progressBarInnerClassName}
-                 answerButtonClassName={this.props.answerButtonClassName}
-                 answerClassName={this.props.answerClassName}
-                 nextQuestionClassName={this.props.nextQuestionClassName}
-                 questionInstructionClassName={this.props.questionInstructionClassName}
-                 questionAnswerListClassName={this.props.questionAnswerListClassName}
-                 quizButtonContainerClassName={this.props.quizButtonContainerClassName}
-    />
+                 onNext={this.onNext} />
   }
-}
-
-QuizContainer.propTypes = {
-  title: React.PropTypes.string,
-  questions: React.PropTypes.array.isRequired,
-  onFinished: React.PropTypes.func.isRequired,
-  progressTextTemplate: React.PropTypes.string,
-  progressBarClassName: React.PropTypes.string,
-  progressBarInnerClassName: React.PropTypes.string,
-  answerButtonClassName: React.PropTypes.string,
-  nextQuestionClassName: React.PropTypes.string,
-  answerClassName: React.PropTypes.string,
-  questionInstructionClassName: React.PropTypes.string,
-  questionAnswerListClassName: React.PropTypes.string,
-  quizButtonContainerClassName: React.PropTypes.string
 }
 
 export default QuizContainer

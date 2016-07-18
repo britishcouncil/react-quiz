f1cc09f773be95e905196d3dd90bbd06baa71c45
“Robin <robin.edman@gmail.com>
2016-07-18 12:23:00 +0100

    Change postversion script to git pull origin dist branch

--- scripts/postversion.sh
+++ scripts/postversion.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+echo "-- Copy files to dist directory"
 cp package.json dist
 cp *.md dist
-git checkout dist
+
+echo "-- Switch to dist branch"
+git checkout dist || git checkout -b dist
+git fetch
+git reset --hard origin/dist
+
+echo "-- Commit the new version"
 cp -a ./dist/ .
 git add .
 git commit -m $(node -p -e "require('./package.json').version")

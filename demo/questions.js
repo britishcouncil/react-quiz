7976a79309dc56e8a1647c24b0940dc15a847fb1
“Robin <robin.edman@gmail.com>
2016-09-08 12:47:32 +0100

    Added multiline question to demo

--- demo/questions.js
+++ demo/questions.js
@@ -18,6 +18,16 @@ const questions = [
       'Here',
       'This'
     ]
+  },
+  {
+    'instruction': 'Complete the following sentence using a word from the list.',
+    text: ['A: This is such an improvement on his last book.', 'B:  I have to agree, that last one was just ____.'],
+    correct: 2,
+    answers: [
+      'approximate',
+      'amusing',
+      'dreadful'
+    ]
   }
 ]
 

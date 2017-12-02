6e96ec575c7123e4acfe016ec22a399b84f6b9ef
Daniele Salvatore <danieleslvtr@gmail.com>
2017-12-02 16:07:27 +0100

    Update to react@16.x, react-dom@16.x, react-hot-loader@3.x Update how PropTypes check is performed using prop-types package Update webpack script to run dev server

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,29 +1,30 @@
 import React from 'react'
 import classNames from 'classnames'
+import PropTypes from 'prop-types';
 
 const ProgressBar = ({ min = 0, max = 100, value = 0, customClassNames = {} }) => {
-  const progressSize = {
-    width: ((value - min) / (max - min) * 100) + '%'
-  }
+    const progressSize = {
+        width: ((value - min) / (max - min) * 100) + '%'
+    }
 
-  const progressBarClassName = classNames('rq-ProgressBar', customClassNames['rq-ProgressBar'])
-  const progressBarInnerClassName = classNames('rq-ProgressBar-inner', customClassNames['rq-ProgressBar-inner'])
+    const progressBarClassName = classNames('rq-ProgressBar', customClassNames['rq-ProgressBar'])
+    const progressBarInnerClassName = classNames('rq-ProgressBar-inner', customClassNames['rq-ProgressBar-inner'])
 
-  // Displays the progress bar size accordingly max, min, current value and isVisible
-  return (
-    <div className={progressBarClassName}>
-      <div className={progressBarInnerClassName} role="progressbar"
-           aria-valuenow={value} aria-valuemin={min} aria-valuemax={max}
-           style={progressSize}>
-      </div>
-    </div>
-  )
+    // Displays the progress bar size accordingly max, min, current value and isVisible
+    return (
+        <div className={progressBarClassName}>
+          <div className={progressBarInnerClassName} role="progressbar"
+               aria-valuenow={value} aria-valuemin={min} aria-valuemax={max}
+               style={progressSize}>
+          </div>
+        </div>
+    )
 }
 
 ProgressBar.propTypes = {
-  min: React.PropTypes.number,
-  max: React.PropTypes.number,
-  value: React.PropTypes.number,
+    min: PropTypes.number,
+    max: PropTypes.number,
+    value: PropTypes.number,
 }
 
 export default ProgressBar
--- src/Quiz.js
+++ src/Quiz.js
@@ -3,54 +3,55 @@ import Question from './Question'
 import ProgressBar from './ProgressBar'
 import classNames from 'classnames'
 import interpolate from 'interpolate'
+import PropTypes from 'prop-types';
 
 const Quiz = ({ questions, answers, currentQuestionIndex, onAnswer, onNext,
-  onFinish, customClassNames = {}, customText = {}}) => {
-  const isLastQuestion = (currentQuestionIndex + 1) === questions.length
+                  onFinish, customClassNames = {}, customText = {}}) => {
+    const isLastQuestion = (currentQuestionIndex + 1) === questions.length
 
-  const progressTextClassName = classNames('rq-Quiz-progressText', customClassNames['rq-Quiz-progressText'])
-  const buttonContainerClassName = classNames('rq-Quiz-buttonContainer', customClassNames['rq-Quiz-buttonContainer'])
-  const nextButtonClassName = classNames('rq-Quiz-nextButton', customClassNames['rq-Quiz-nextButton'])
+    const progressTextClassName = classNames('rq-Quiz-progressText', customClassNames['rq-Quiz-progressText'])
+    const buttonContainerClassName = classNames('rq-Quiz-buttonContainer', customClassNames['rq-Quiz-buttonContainer'])
+    const nextButtonClassName = classNames('rq-Quiz-nextButton', customClassNames['rq-Quiz-nextButton'])
 
-  const finishButtonText = customText['rq-Quiz-nextButton--finish'] || 'Finish'
-  const nextButtonText = customText['rq-Quiz-nextButton'] || 'Next'
-  const progressText = interpolate(customText['rq-Quiz-progressText'] || 'Question {n} of {total}', {
-    n: currentQuestionIndex + 1,
-    total: questions.length
-  })
+    const finishButtonText = customText['rq-Quiz-nextButton--finish'] || 'Finish'
+    const nextButtonText = customText['rq-Quiz-nextButton'] || 'Next'
+    const progressText = interpolate(customText['rq-Quiz-progressText'] || 'Question {n} of {total}', {
+        n: currentQuestionIndex + 1,
+        total: questions.length
+    })
 
-  return (
-    <div>
-      <p className={progressTextClassName}>{progressText}</p>
-      <ProgressBar value={currentQuestionIndex + 1}
-                   max={questions.length}
-                   customClassNames={customClassNames} />
-      <Question customClassNames={customClassNames}
-                onAnswer={onAnswer}
-                selectedAnswer={answers[currentQuestionIndex]}
-                {...questions[currentQuestionIndex]} />
-      <div className={buttonContainerClassName}>
-        {isLastQuestion
-          ? <button className={nextButtonClassName}
-                    onClick={() => onFinish(answers)}
-                    disabled={answers[currentQuestionIndex] === undefined}>{finishButtonText}</button>
-          : <button className={nextButtonClassName}
-                    onClick={onNext}
-                    disabled={answers[currentQuestionIndex] === undefined}>{nextButtonText}</button>
-        }
-      </div>
-    </div>
-  )
+    return (
+        <div>
+          <p className={progressTextClassName}>{progressText}</p>
+          <ProgressBar value={currentQuestionIndex + 1}
+                       max={questions.length}
+                       customClassNames={customClassNames} />
+          <Question customClassNames={customClassNames}
+                    onAnswer={onAnswer}
+                    selectedAnswer={answers[currentQuestionIndex]}
+                    {...questions[currentQuestionIndex]} />
+          <div className={buttonContainerClassName}>
+              {isLastQuestion
+                  ? <button className={nextButtonClassName}
+                            onClick={() => onFinish(answers)}
+                            disabled={answers[currentQuestionIndex] === undefined}>{finishButtonText}</button>
+                  : <button className={nextButtonClassName}
+                            onClick={onNext}
+                            disabled={answers[currentQuestionIndex] === undefined}>{nextButtonText}</button>
+              }
+          </div>
+        </div>
+    )
 }
 
 Quiz.propTypes = {
-  questions: React.PropTypes.array.isRequired,
-  answers: React.PropTypes.object.isRequired,
-  currentQuestionIndex: React.PropTypes.number.isRequired,
-  onAnswer: React.PropTypes.func.isRequired,
-  onNext: React.PropTypes.func.isRequired,
-  onFinish: React.PropTypes.func.isRequired,
-  progressTextTemplate: React.PropTypes.string
+    questions: PropTypes.array.isRequired,
+    answers: PropTypes.object.isRequired,
+    currentQuestionIndex: PropTypes.number.isRequired,
+    onAnswer: PropTypes.func.isRequired,
+    onNext: PropTypes.func.isRequired,
+    onFinish: PropTypes.func.isRequired,
+    progressTextTemplate: PropTypes.string
 }
 
 export default Quiz
